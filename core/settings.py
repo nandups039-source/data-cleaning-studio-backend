d4596fb4786b07bd3b34db94c6d52a7f98a8c057
nandups039-source <nandups039@gmail.com>
2026-01-10 01:52:33 +0400

    Added allowed hosts in settings

--- core/settings.py
+++ core/settings.py
@@ -15,12 +15,6 @@ from pathlib import Path
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config(
-    "ALLOWED_HOSTS",
-    default="*",
-    cast=lambda v: [s.strip() for s in v.split(",")]
-)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -34,7 +28,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
